2b26d60d41d4938d1e7625d13a6f452afb739b1e
Janie <evgeniyaberseneva89@gmail.com>
2019-05-23 17:23:43 +0300

    recipe list done

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -2,13 +2,25 @@ import React, { Component } from "react";
 import Recipe from "./Recipe";
 
 class RecipeList extends Component {
-  state = {};
   render() {
+    const { recipes } = this.props;
     return (
-      <div>
-        <h1>Hello from Recipe List</h1>
-        <Recipe />
-      </div>
+      <>
+        <div className="container">
+          {/* title */}
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase my-3">
+              <h1 className="text-slanted">recipe list</h1>
+            </div>
+          </div>
+          {/* end of title */}
+          <div className="row">
+            {recipes.map(recipe => (
+              <Recipe key={recipe.recipe_id} recipe={recipe} />
+            ))}
+          </div>
+        </div>
+      </>
     );
   }
 }
